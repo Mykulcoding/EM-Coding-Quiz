813a53ab33f49da471fd823622b37c84418b1a4a
MykulCoding <Marm2@github.com>
2023-12-01 21:48:20 +0000

    added pseudocode

---
+++ assets/logic.js
@@ -0,0 +1,52 @@
+//PSEUDOCODE
+
+// HTML elements
+// - Start button
+// - Questions container
+// - Timer display
+// - End screen for initials and score input
+// - Highscores display screen
+
+// Global Variables
+// - Questions array with objects containing question, choices, and correct answer
+// - Timer variable
+// - Score variable
+// - Current question index
+// - Event listeners for start button and choice selection
+
+// Function to start the quiz
+  // Hide start screen
+  // Show questions container
+  // Start timer
+  // Display current question and choices
+
+// Function to display questions
+  // Display question text
+  // Display choices as buttons/options
+
+// Function to handle answer selection
+  // Check if the selected answer is correct
+  // If correct, increment score
+  // If incorrect, deduct time from the timer
+
+// Function to end the quiz
+  // Stop the timer
+  // Show end screen with final score
+  // Prompt for initials input and save score
+
+// Function to save highscores
+  // Store initials and score in local storage
+  // Display highscores on the highscores.html page
+
+// Function to load highscores
+  // Retrieve highscores from local storage
+  // Display highscores on the highscores.html page
+
+// Event listeners
+// - Start button click starts the quiz
+// - Choice selection triggers answer handling
+// - Submit button at the end saves highscores
+
+// Function to handle highscore clearing
+  // Clear highscores from local storage
+  // Update highscores display
