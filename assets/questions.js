97735161b18b00c58e98b8b7c2b43449f5b4f1c3
MykulCoding <Marm2@github.com>
2023-12-01 22:12:02 +0000

    added questions.js code repeating 10 questions to update accordingly later

---
+++ assets/questions.js
@@ -0,0 +1,55 @@
+const questions = [
+    {
+        question: "What is JavaScript?",
+        choices: ["A programming language", "A markup language", "A style sheet language"],
+        answer: "A programming language"
+    },
+    {
+        question: "What is JavaScript?",
+        choices: ["A programming language", "A markup language", "A style sheet language"],
+        answer: "A programming language"
+    },
+    {
+        question: "What is JavaScript?",
+        choices: ["A programming language", "A markup language", "A style sheet language"],
+        answer: "A programming language"
+    },
+    {
+        question: "What is JavaScript?",
+        choices: ["A programming language", "A markup language", "A style sheet language"],
+        answer: "A programming language"
+    },
+    {
+        question: "What is JavaScript?",
+        choices: ["A programming language", "A markup language", "A style sheet language"],
+        answer: "A programming language"
+    },
+    {
+        question: "What is JavaScript?",
+        choices: ["A programming language", "A markup language", "A style sheet language"],
+        answer: "A programming language"
+    },
+    {
+        question: "What is JavaScript?",
+        choices: ["A programming language", "A markup language", "A style sheet language"],
+        answer: "A programming language"
+    },
+    {
+        question: "What is JavaScript?",
+        choices: ["A programming language", "A markup language", "A style sheet language"],
+        answer: "A programming language"
+    },
+    {
+        question: "What is JavaScript?",
+        choices: ["A programming language", "A markup language", "A style sheet language"],
+        answer: "A programming language"
+    },
+    {
+        question: "What is JavaScript?",
+        choices: ["A programming language", "A markup language", "A style sheet language"],
+        answer: "A programming language"
+    },
+
+];
+
+export default questions;
