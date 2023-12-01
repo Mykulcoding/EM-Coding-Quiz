9c01ea5530e307d4a4da5b4764fa6c63d2096358
MykulCoding <Marm2@github.com>
2023-12-01 23:22:15 +0000

    updated 10 questions on the qustions.js file

--- assets/questions.js
+++ assets/questions.js
@@ -5,49 +5,49 @@ const questions = [
         answer: "A programming language"
     },
     {
-        question: "What is JavaScript?",
-        choices: ["A programming language", "A markup language", "A style sheet language"],
-        answer: "A programming language"
+        question: "Which of the following correctly executes three instructions if the condition is true?",
+        choices: ["If (x < 0) a = b * 2; y = x; z = a – y;", "If (x < 0) { a = b * 2; y = x; z = a – y; }", "If { (x < 0) a = b * 2; y = x; z = a – y ; }"],
+        answer: "If (x < 0) { a = b * 2; y = x; z = a – y; }"
     },
     {
-        question: "What is JavaScript?",
-        choices: ["A programming language", "A markup language", "A style sheet language"],
-        answer: "A programming language"
+        question: "What is the average length of a banana?",
+        choices: ["7 inches", "5 inches", "10 inches"],
+        answer: "7 inches"
     },
     {
-        question: "What is JavaScript?",
-        choices: ["A programming language", "A markup language", "A style sheet language"],
-        answer: "A programming language"
+        question: "Which language is commonly used for front-end web development?",
+        choices: ["Java", "Python", "HTML"],
+        answer: "HTML"
     },
     {
-        question: "What is JavaScript?",
-        choices: ["A programming language", "A markup language", "A style sheet language"],
-        answer: "A programming language"
+        question: "What does CSS stand for?",
+        choices: ["Cascading Style Sheets", "Cascading Style Sheets", "A style sheet language"],
+        answer: "Cascading Style Sheets"
     },
     {
-        question: "What is JavaScript?",
-        choices: ["A programming language", "A markup language", "A style sheet language"],
-        answer: "A programming language"
+        question: "Which programming language is known for its simplicity and readability?",
+        choices: ["C++", "Ruby", "Swift"],
+        answer: "Ruby"
     },
     {
-        question: "What is JavaScript?",
-        choices: ["A programming language", "A markup language", "A style sheet language"],
-        answer: "A programming language"
+        question: "What does SQL stand for?",
+        choices: ["Structured Query Language", "Simple Query Logic", "Systematic Query Link"],
+        answer: "Structured Query Language"
     },
     {
-        question: "What is JavaScript?",
-        choices: ["A programming language", "A markup language", "A style sheet language"],
-        answer: "A programming language"
+        question: "Which language is commonly used for server-side scripting?",
+        choices: ["JavaScript", "PHP", "Python"],
+        answer: "PHP"
     },
     {
-        question: "What is JavaScript?",
-        choices: ["A programming language", "A markup language", "A style sheet language"],
-        answer: "A programming language"
+        question: "What is the purpose of a loop in programming?",
+        choices: ["To create graphics", "To store data", "To repeat a set of instructions"],
+        answer: "To repeat a set of instructions"
     },
     {
-        question: "What is JavaScript?",
-        choices: ["A programming language", "A markup language", "A style sheet language"],
-        answer: "A programming language"
+        question: "Which data structure follows the Last In, First Out (LIFO) principle?",
+        choices: ["Queue", "Stack", "Linked List"],
+        answer: "Stack"
     },
 
 ];
