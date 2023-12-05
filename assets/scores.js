d7722e3672e4869d574bf79556855555c6a0cbaf
MykulCoding <Marm2@github.com>
2023-12-05 19:30:12 +0000

    added notes to my scores.js, updated the highscores.html with path to scores.js (updated)

--- assets/scores.js
+++ assets/scores.js
@@ -1,18 +1,43 @@
+// Function to load highscores
 function loadHighscores() {
-    const highscoresList = document.getElementById('highscores');
-    const highscores = JSON.parse(localStorage.getItem('highscores')) || [];
-    highscores.sort((a, b) => b.score - a.score); // Sort scores in descending order
-    highscores.forEach(entry => {
-      const li = document.createElement('li');
-      li.textContent = `${entry.initials} - ${entry.score}`;
-      highscoresList.appendChild(li);
-    });
+  // Get the element with the ID 'highscores'
+  const highscoresList = document.getElementById('highscores');
+
+  // Check if the 'highscores' element exists
+  if (!highscoresList) {
+    // If the element doesn't exist, log an error and return from the function
+    console.error("Highscores list element not found");
+    return;
   }
-  
-  function clearHighscores() {
-    const highscoresList = document.getElementById('highscores');
-    localStorage.removeItem('highscores');
-    highscoresList.innerHTML = '';
-  }
-  
-  document.addEventListener('DOMContentLoaded', loadHighscores);
+
+  // Log the 'highscores' list element to the console
+  console.log("Highscores List Element:", highscoresList);
+
+  // Retrieve highscores from localStorage or initialize an empty array
+  const highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+
+  // Sort the highscores array in descending order based on the 'score' property
+  highscores.sort((a, b) => b.score - a.score);
+
+  // Clear the contents of the 'highscores' list
+  highscoresList.innerHTML = '';
+
+  // Iterate through the highscores array
+  highscores.forEach(entry => {
+    // Create a new list item element for each entry
+    const li = document.createElement('li');
+    // Set the text content of the list item to display initials and score
+    li.textContent = `${entry.initials} - ${entry.score}`;
+    // Append the list item to the 'highscores' list
+    highscoresList.appendChild(li);
+  });
+}
+
+// Event listener for the 'DOMContentLoaded' event
+document.addEventListener('DOMContentLoaded', () => {
+  // Log that the 'DOMContentLoaded' event has been fired
+  console.log("DOMContentLoaded event fired");
+
+  // Call the 'loadHighscores' function when the DOM has finished loading
+  loadHighscores();
+});
